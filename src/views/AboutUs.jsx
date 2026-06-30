b14c71af1bf63d741ca5e55437e699007541d61f
dushyant kumar <dushyantvelar@gmail.com>
2026-06-30 12:03:20 +0530

    added the about section of the project

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -1,46 +1,26 @@
-import React from "react";
-import User from "../components/user";
-import ClassUser from "../components/ClassUser";
-import useAllRestaurant from "../hooks/useAllRestaurant";
-
-// class AboutUs extends React.Component {
-//   constructor(prop){
-//     super(prop);
-//   }
-
-//   componentDidMount(){
-//     // console.log("parent mount ")
-//   }
-//   render() {
-//     // console.log("parent render");
-//     return (
-//       <div className="p-2">
-//         <h1>About Us</h1>
-//         <div>this is a food delivery webapp</div>
-//         {/* <User name={"dushyant"} location={"mainpuri"} contact={"dushyantkumar4"}/> */}
-//         <ClassUser
-//           name={"class"}
-//           location={"classLocation"}
-//           contact={"developerclass"}
-//         />
-//       </div>
-//     );
-//   }
-// }
-
 const AboutUs = () => {
-
-  const allres = useAllRestaurant();
-  console.log(allres);
   return (
-    <div className='p-2'>
-        <h1>About Us</h1>
-        <div>this is a food delivery webapp</div>
-        {/* <User name={"dushyant"} location={"mainpuri"} contact={"dushyantkumar4"}/> */}
-
-        <ClassUser name={"class"} location={"classLocation"} contact={"developerclass"}/>
+    <div className="flex flex-col px-15 py-5 gap-5">
+      <h1 className="text-3xl font-bold">About DecentFood</h1>
+      <p className="font-medium">
+        Welcome to DecentFood, your simple and reliable food ordering platform.
+        Our goal is to make discovering and ordering delicious meals quick,
+        easy, and hassle-free.
+      </p>
+      <p className="font-medium">
+        Whether you're craving your favorite dishes or exploring new
+        restaurants, DecentFood connects you with a variety of food options
+        through a smooth and user-friendly experience. From browsing menus to
+        placing orders, every step is designed to be fast and convenient.
+      </p>
+      <p className="font-medium">
+        At DecentFood, we believe that great food should be just a few clicks
+        away. We're committed to providing a seamless ordering experience while
+        helping local restaurants reach more customers.
+      </p>
+      <p className="font-medium">Fresh food. Fast delivery. Happy customers.</p>
     </div>
-  )
-}
+  );
+};
 
 export default AboutUs;
