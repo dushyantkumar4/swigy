b60c14dd0f3f4b2e28919d35ce5e152a504546d4
dushyant kumar <dushyantvelar@gmail.com>
2026-07-10 09:39:41 +0530

    feat:added the clear cart functionality with conditional rendring

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,20 +2,58 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { IMG_URL } from "../utils/constant.js";
 import { useDispatch } from "react-redux";
-import { removeItem } from "../utils/cartSlice.js";
+import { removeItem, clearItem } from "../utils/cartSlice.js";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const carts = useSelector((store) => store.cart.item);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const handleRemoveItem = (id)=>{
+  // remove single item from cart
+  const handleRemoveItem = (id) => {
     dispatch(removeItem(id));
+  };
+  // Complete clear cart store
+  const handleClearCart = () => {
+    dispatch(clearItem());
   };
 
   return (
-    <div className="text-center flex flex-col items-center mt-4">
+    <div className=" flex flex-col items-center gap-5 mt-4">
+      {carts.length === 0 ? (
+        <div className=" flex flex-col items-center justify-center gap-5 mt-5">
+          <h1 className="text-2xl font-bold">Cart</h1>
+          <div className=" flex flex-col items-center gap-10 ">
+            <p className="text-xl text-gray-400">
+              No items available first go to restaurant & select the item from
+              Categories
+            </p>
+            <button
+              onClick={() => navigate("/")}
+              className="text-green-600 sm:font-medium md:font-semibold border border-gray-300 text-center w-auto px-5 py-1 shadow-lg rounded-lg"
+            >
+              Select
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="flex justify-between items-center w-6/12">
+          <h1 className="text-2xl font-bold">Cart</h1>
+          <button
+            onClick={handleClearCart}
+            className="text-green-600 sm:font-medium md:font-semibold border border-gray-300 text-center w-auto px-5 py-1 shadow-lg rounded-lg"
+          >
+            Clear Cart
+          </button>
+        </div>
+      )}
+
       {carts.map((cart) => (
-        <div key={cart.id} className="flex justify-between w-6/12 px-1 border-b border-gray-300 pb-5">
+        <div
+          key={cart.id}
+          className="flex justify-between w-6/12 px-1 border-b border-gray-300 pb-5"
+        >
           <div className="flex flex-col justify-around">
             <p className="text-lg font-semibold">{cart.dishName}</p>
             <p>₹ {cart.price / 100 || "price not available"}</p>
@@ -28,7 +66,10 @@ const Cart = () => {
                 className="w-full h-full"
               />
             </div>
-            <button onClick={()=>handleRemoveItem(cart.id)} className=" text-green-600 sm:font-medium md:font-semibold border border-gray-300 text-center w-auto px-5 py-1 shadow-lg rounded-lg">
+            <button
+              onClick={() => handleRemoveItem(cart.id)}
+              className=" text-green-600 sm:font-medium md:font-semibold border border-gray-300 text-center w-auto px-5 py-1 shadow-lg rounded-lg"
+            >
               Remove
             </button>
           </div>
