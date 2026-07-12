992292143c19af823c03725567ae15acaaabddf5
dushyant kumar <dushyantvelar@gmail.com>
2026-07-12 21:44:25 +0530

    added the contect compo content

--- src/views/Contacts.jsx
+++ src/views/Contacts.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Contacts = () => {
   return (
     <div>
-        <h1>Contact Us</h1>
-        <p>this is our contat page </p>
+      <h1 className="text-3xl font-bold p-4 m-4">Contact Us</h1>
+      <form action="">
+        <input
+          type="text"
+          className="border border-black rounded-lg p-2 m-2"
+          placeholder="name"
+        />
+        <input
+          type="text"
+          className="border border-black rounded-lg p-2 m-2"
+          placeholder="Message"
+        />
+        <button className="border border-black p-2 rounded-lg bg-gray-100 text-green-600">
+          Submit
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Contacts
+export default Contacts;
