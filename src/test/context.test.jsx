7b69878b9120cf9eb2e8c84754b889aff0fcc1c7
dushyant kumar <dushyantvelar@gmail.com>
2026-07-12 21:47:19 +0530

    Feat: added the test files & write test cases

---
+++ src/test/context.test.jsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import Contacts from "../views/Contacts";
+import { describe } from "vitest";
+
+describe("Contact us page test cases", () => {
+  it("should load contact us component", () => {
+    render(<Contacts />);
+    //   Querying
+    const heading = screen.getByRole("heading");
+    //   Assertion
+    expect(heading).toBeInTheDocument();
+  });
+  it("should load button inside contact us component", () => {
+    render(<Contacts />);
+    //   Querying
+    const button = screen.getByText("Submit");
+    //   Assertion
+    expect(button).toBeInTheDocument();
+  });
+  it("should load Inputname inside contact us component", () => {
+    render(<Contacts />);
+    //   Querying
+    const inputName = screen.getByPlaceholderText("name");
+    //   Assertion
+    expect(inputName).toBeInTheDocument();
+  });
+  it("should return all input element in contect compo", () => {
+    render(<Contacts />);
+    //   Querying
+    const inputBoxes = screen.getAllByRole("textbox");
+    //   console.log(inputBoxes);
+    // Assertion
+    expect(inputBoxes.length).toBe(2);
+  });
+});
