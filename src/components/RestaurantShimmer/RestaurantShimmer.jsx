bf556fd57e113410e77e681fa3a6121b14852de2
dushyant kumar <dushyantvelar@gmail.com>
2026-07-22 16:39:40 +0530

    remove the repetition for the shimmer code

--- src/components/RestaurantShimmer/RestaurantShimmer.jsx
+++ src/components/RestaurantShimmer/RestaurantShimmer.jsx
@@ -1,99 +1,25 @@
 import React from "react";
 import { ShimmerPostItem } from "react-shimmer-effects";
 import "./RestaurantShimmer.css";
+const count = 8;
 
 const RestaurantShimmer = () => {
   return (
     <div className="shimmer-container">
-      <ShimmerPostItem
-        className="shimmer-card"
-        style={{ marginTop: "2rem",paddingTop:"2rem" }}
-        card
-        title
-        text
-        cta
-        imageType="thumbnail"
-        imageWidth={200}
-        imageHeight={200}
-        contentCenter
-      />
-      <ShimmerPostItem
-        card
-        title
-        text
-        cta
-        imageType="thumbnail"
-        imageWidth={200}
-        imageHeight={200}
-        contentCenter
-        className="shimmer-card"
-      />
-      <ShimmerPostItem
-        card
-        title
-        text
-        cta
-        imageType="thumbnail"
-        imageWidth={200}
-        imageHeight={200}
-        contentCenter
-        className="shimmer-card"
-      />
-      <ShimmerPostItem
-        card
-        title
-        text
-        cta
-        imageType="thumbnail"
-        imageWidth={200}
-        imageHeight={200}
-        contentCenter
-        className="shimmer-card"
-      />
-      <ShimmerPostItem
-        card
-        title
-        text
-        cta
-        imageType="thumbnail"
-        imageWidth={200}
-        imageHeight={200}
-        contentCenter
-        className="shimmer-card"
-      />
-      <ShimmerPostItem
-        card
-        title
-        text
-        cta
-        imageType="thumbnail"
-        imageWidth={200}
-        imageHeight={200}
-        contentCenter
-        className="shimmer-card"
-      />
-      <ShimmerPostItem
-        card
-        title
-        text
-        cta
-        imageType="thumbnail"
-        imageWidth={200}
-        imageHeight={200}
-        contentCenter
-        className="shimmer-card"
-      />
-      <ShimmerPostItem
-        card
-        title
-        text
-        cta
-        imageType="thumbnail"
-        imageWidth={200}
-        imageHeight={200}
-        contentCenter
-        className="shimmer-card"
-      />
+      {Array.from({ length: count }).map((_, index) => (
+        <ShimmerPostItem
+          className="shimmer-card"
+          style={{ marginTop: "2rem", paddingTop: "2rem" }}
+          card
+          title
+          text
+          cta
+          imageType="thumbnail"
+          imageWidth={200}
+          imageHeight={200}
+          contentCenter
+        />
+      ))}
     </div>
   );
 };
